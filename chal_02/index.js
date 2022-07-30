8b0b82fb1a6d359046acdb232fd6b5bbb823b0cb
Haen <nd589@georgetown.edu>
2022-07-30 12:49:32 -0400

    Add js and css corrections

--- chal_02/index.js
+++ chal_02/index.js
@@ -1,26 +1,29 @@
 
-const activeBtns = document.querySelectorAll('.rate-bttn');
-
-activeBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        activeBtns.forEach(f => f != e.target ? f.classList.remove('active') : '');
-        e.target.classList.toggle('active');
-    });
-});
-//this works from solution "toggle active class to buttons when clicked"
-/* solution selects and leaves in active state
-but doesnt remove active state if another button is selected */
-//prob is I have no idea how it works ... but I can learn !
- 
-var ele = document.querySelector('.active');
- document.getElementById('number-selected').textContent += 
- `You selected ${(ele)} out of 5`;
-
- //might just need to use button as submit form ... this would be a simple solution I'v overlooked haha
-
- //PsuedoCode
- /*Simplest Way:
- find element wtih active class getElementbyId
- pull id from element with active class 
- place id into ${} above
+//Using guide from "SankThomas" for javascript submit
+/*Main Changes"
+1. using hover/focus states for button in css instead of js
+2. ratings.forEach function:
+    for each btn, whenever clicked, the function changes the actual rating content to
+    id rating containing a number
+    note that ratings refers to the btns, actualRating fefers to var made by #ratings
  */
+
+const submitButton = document.getElementById('sub-rating')
+const ratings = document.querySelectorAll('.btn')
+const actualRating = document.getElementById('rating')
+const rateContainer = document.querySelector('.rate-container')
+const thanksContainer = document.querySelector('.thank-container')
+
+
+submitButton.addEventListener('click', () =>{
+    submitButton.addEventListener('click', () => {
+        rateContainer.style.display = 'none'
+        thanksContainer.classList.remove('hidden')
+            })
+
+    ratings.forEach((rating) => {
+        rating.addEventListener('click', () => {
+            actualRating.textContent = rating.textContent
+        })
+    })
+})
